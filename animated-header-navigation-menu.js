b7ba88ed479a44183bdd691246f4c6cb1e5a3d77
sabriflorin <sabriflorin@gmail.com>
2023-07-14 18:53:57 +0200

    Update animated-header-navigation-menu.js

--- animated-header-navigation-menu.js
+++ animated-header-navigation-menu.js
@@ -15,42 +15,39 @@
   <span class="hidden"><a href="#">Home </a></span><span class="hidden"><a href="#">About </a></span><span class="hidden"><a href="#">Info</a></span><span class="hidden"><a href="#">Contact</a></span>
 </div>
 */
-(function() {
-  'use strict';
-  function addNavHeader() {
-      console.info('INIT NAV HEADER BOOKMARK')
-      const menuDiv = document.createElement('div')
-      menuDiv.classList.add('.menu-nav-header')
-      const containerDiv = document.createElement('div')
-      containerDiv.classList.add('container-nav-header')
-      const toggleDiv = document.createElement('div')
-      toggleDiv.classList.add('toggle-nav-header')
-      containerDiv.append(toggleDiv)
-      menuDiv.append(containerDiv)
-  
-      const spanHidden = document.createElement('span')
-      spanHidden.classList.add('hidden-nav-header')
-      spanHidden.classList.add('span-nav-header')
-      spanHidden.innerHTML = 'Test menu'
-      menuDiv.append(spanHidden)
+function addNavHeader() {
+    console.info('INIT NAV HEADER BOOKMARK')
+    const menuDiv = document.createElement('div')
+    menuDiv.classList.add('.menu-nav-header')
+    const containerDiv = document.createElement('div')
+    containerDiv.classList.add('container-nav-header')
+    const toggleDiv = document.createElement('div')
+    toggleDiv.classList.add('toggle-nav-header')
+    containerDiv.append(toggleDiv)
+    menuDiv.append(containerDiv)
 
-      const body = document.querySelector('body')
-      body.append(menuDiv)
-      console.info('ADDED ELEMENT NAV HEADER BOOKMARK')
-  
-      const eltToggle = document.querySelector('.toggle-nav-header');
-      eltToggle.addEventListener('click', function() {
-        const eltMenu = document.querySelector('.menu-nav-header');
-        const eltSpan = document.querySelector('span.hidden-nav-header');
-        const eltContainer = document.querySelector('.container-nav-header');
-        eltMenu.classList.toggle('expanded-nav-header');
-        eltSpan.classList.toggle('hidden-nav-header');
-        eltContainer.classList.toggle('close-nav-header');
-        eltToggle.classList.toggle('close-nav-header');
-      })
-      console.info('ADDED LISTENER NAV HEADER BOOKMARK')
-  }
-})
+    const spanHidden = document.createElement('span')
+    spanHidden.classList.add('hidden-nav-header')
+    spanHidden.classList.add('span-nav-header')
+    spanHidden.innerHTML = 'Test menu'
+    menuDiv.append(spanHidden)
+
+    const body = document.querySelector('body')
+    body.append(menuDiv)
+    console.info('ADDED ELEMENT NAV HEADER BOOKMARK')
+
+    const eltToggle = document.querySelector('.toggle-nav-header');
+    eltToggle.addEventListener('click', function() {
+      const eltMenu = document.querySelector('.menu-nav-header');
+      const eltSpan = document.querySelector('span.hidden-nav-header');
+      const eltContainer = document.querySelector('.container-nav-header');
+      eltMenu.classList.toggle('expanded-nav-header');
+      eltSpan.classList.toggle('hidden-nav-header');
+      eltContainer.classList.toggle('close-nav-header');
+      eltToggle.classList.toggle('close-nav-header');
+    })
+    console.info('ADDED LISTENER NAV HEADER BOOKMARK')
+}
 /*
 $('.toggle').on('click', function() {
   $('.menu').toggleClass('expanded');  
