4d0788b03d1b4009643c09ac9fe518e741310081
sabriflorin <sabriflorin@gmail.com>
2023-07-16 00:35:13 +0200

    Update sidebar-navigation.js

--- sidebar-navigation.js
+++ sidebar-navigation.js
@@ -11,29 +11,29 @@ class SideBar {
 		</div>
 </div>`;
 
-    this.addButtonToDOM = this.addButtonToDOM.bind(this);
-    this.init = this.init.bind(this);
-    this.init()
-  }
-
-  init() {
     document.body.insertAdjacentHTML( 'beforeend', this.sidebar );
-    const sidebarBtn = document.querySelector('#btn');
-    sidebarBtn.addEventListener('click', event => {
-      sidebarBtn.classList.toggle('active');
-      sidebarBox.classList.toggle('active');
-    });
+
+      const sidebarBtn = document.querySelector('#btn-bookmark');
+      const sidebarBox = document.querySelector('#box-bookmark');
+      sidebarBtn.addEventListener('click', event => {
+        sidebarBtn.classList.toggle('active');
+        sidebarBox.classList.toggle('active');
+      });
+
+      /*
+      document.body.addEventListener('click', event => {
+        if (sidebarBox.classList.contains('active')) {
+          sidebarBtn.classList.remove('active');
+          sidebarBox.classList.remove('active');
+        }
+      });
+      */
     
-    document.body.addEventListener('click', event => {
-      if (sidebarBox.classList.contains('active')) {
-        sidebarBtn.classList.remove('active');
-        sidebarBox.classList.remove('active');
-      }
-    });
+    this.addButtonToDOM = this.addButtonToDOM.bind(this);
   }
 
   addButtonToDOM() {
-    const itemsNode = document.querySelector('#page-wrapper');
+    const itemsNode = document.querySelector('#items-bookmark');
 		for (var i = 0; i < this.list.length; i++) {
 			const itemNode = document.createElement('div');
 			itemNode.classList.add('item-bookmark');
