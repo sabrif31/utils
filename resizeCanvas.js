2a7ac78c2178531463f59d29308cf4331792e187
sabriflorin <sabriflorin@gmail.com>
2023-07-19 13:10:51 +0200

    test2

--- resizeCanvas.js
+++ resizeCanvas.js
@@ -4,9 +4,42 @@
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.canvasUtils = factory());
   })(this, (function () { 'use strict';
 
+    var _downKeys = []; // 记录摁下的绑定键
+    var winListendFocus = false; // window是否已经监听了focus事件
+    var _scope = 'all'; // 默认热键范围
+    var elementHasBindEvent = []; // 已绑定事件的节点记录
 
     function resizeCanvas() {
         console.log('resizeCanvas')
+    }
+
+    function getKeys(key) {
+        if (typeof key !== 'string') key = '';
+        key = key.replace(/\s/g, '');
+        var keys = key.split(',');
+        var index = keys.lastIndexOf('');
+    
+        for (; index >= 0;) {
+          keys[index - 1] += ',';
+          keys.splice(index, 1);
+          index = keys.lastIndexOf('');
+        }
+        return keys;
+      }
+
+    function canvasUtils() {
+        console.log('INIT')
+        _downKeys = [];
+        var keys = getKeys(key);
+        var mods = [];
+        var scope = 'all';
+        var element = document;
+        var i = 0;
+        var keyup = false;
+        var keydown = true;
+        var splitKey = '+';
+        var capture = false;
+        console.log('keys', keys)
     }
   
     var _api = {
