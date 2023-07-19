0b1b8cabf99e5f8f3581850d4adb350b7df32b6e
sabriflorin <sabriflorin@gmail.com>
2023-07-19 13:23:22 +0200

    teeeeeeeeet

--- resizeCanvas.js
+++ resizeCanvas.js
@@ -9,8 +9,30 @@
     var _scope = 'all'; // 默认热键范围
     var elementHasBindEvent = []; // 已绑定事件的节点记录
 
-    function resizeCanvas() {
-        console.log('resizeCanvas')
+    /**
+    * USE RESIZE CANVAS
+    * <canvas id="my-canvas" width="480" height="270" />
+    * const canvasCtx = canvas.getContext("2d")
+    * const { xOffset, yOffset, newWidth, newHeight } = resizeCanvas(video, canvas);
+    * canvasCtx.drawImage(video, xOffset, yOffset, newWidth, newHeight);
+    */
+    function resizeCanvas(srcNode, canvasNode) {
+        var wrh = srcNode.width / srcNode.height;
+        var newWidth = canvasNode.width;
+        var newHeight = newWidth / wrh;
+        if (newHeight > canvasNode.height) {
+            newHeight = canvasNode.height;
+            newWidth = newHeight * wrh;
+        }
+        var xOffset = newWidth < canvasNode.width ? ((canvasNode.width - newWidth) / 2) : 0;
+        var yOffset = newHeight < canvasNode.height ? ((canvasNode.height - newHeight) / 2) : 0;
+
+        return {
+            xOffset,
+            yOffset,
+            newWidth,
+            newHeight
+        }
     }
 
     function getKeys(key) {
