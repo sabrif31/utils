1dfb790000c3895ca71408f738566535e65c978d
sabriflorin <sabriflorin@gmail.com>
2023-07-20 12:40:25 +0200

    YTB

--- sidebar-navigation.js
+++ sidebar-navigation.js
@@ -75,10 +75,11 @@ class SideBar {
       const itemsNode = document.querySelector('#items-bookmark');
       for (var i = 0; i < this.list.length; i++) {
           const divElt = document.createElement('div');
+          divElt.classList.add('list-navigation');
 
           const img = document.createElement('img');
           img.altText = "Thumbnail Video"
-          img.src =this.list?.[i]?.thumbnail;
+          img.src = this.list?.[i]?.thumbnail;
           img.width = 250
           divElt.append(img);
 
